fda9113898acc548e14f7bc486f049581480bc71
karabas94 <valet14051994@gmail.com>
2022-11-12 01:00:03 +0200

    Exersice updated

--- exercise_1.py
+++ exercise_1.py
@@ -2,13 +2,14 @@ class Transport:
     """
     Creating master class of Transport
     """
+
     def __init__(self, number_of_transport, number_of_wheels, power_of_engine, max_speed):
         """
-        Initialization attributes:
-        :param number_of_transport:
-        :param number_of_wheels:
-        :param power_of_engine:
-        :param max_speed:
+        Initialization attributes of parameters:
+        -number_of_transport:
+        -number_of_wheels:
+        -power_of_engine:
+        -max_speed:
         """
         self.number_of_transport = number_of_transport
         self.number_of_wheels = number_of_wheels
@@ -27,14 +28,15 @@ class Bicycle(Transport):
     """
     Creating slave class Bicycle from master class Transport
     """
+
     def __init__(self, number_of_transport, number_of_wheels, power_of_engine, max_speed, bicycle_brand):
         """
-        Initialization attributes:
-        :param number_of_transport:
-        :param number_of_wheels:
-        :param power_of_engine:
-        :param max_speed:
-        :param bicycle_brand:
+        Initialization attributes of parameters:
+        -number_of_transport:
+        -number_of_wheels:
+        -power_of_engine:
+        -max_speed:
+        -bicycle_brand:
         """
         super().__init__(number_of_transport, number_of_wheels, power_of_engine, max_speed)
         self.bicycle_brand = bicycle_brand
@@ -51,14 +53,15 @@ class Car(Transport):
     """
     Creating slave class Car from master class Transport
     """
+
     def __init__(self, number_of_transport, number_of_wheels, power_of_engine, max_speed, car_brand):
         """
-        Initialization attributes:
-        :param number_of_transport:
-        :param number_of_wheels:
-        :param power_of_engine:
-        :param max_speed:
-        :param car_brand:
+        Initialization attributes of parameters:
+        -number_of_transport:
+        -number_of_wheels:
+        -power_of_engine:
+        -max_speed:
+        -car_brand:
         """
         super().__init__(number_of_transport, number_of_wheels, power_of_engine, max_speed)
         self.car_brand = car_brand
@@ -75,14 +78,15 @@ class Bus(Transport):
     """
     Creating slave class Bus from master class Transport
     """
+
     def __init__(self, number_of_transport, number_of_wheels, power_of_engine, max_speed, bus_brand):
         """
-        Initialization attributes:
-        :param number_of_transport:
-        :param number_of_wheels:
-        :param power_of_engine:
-        :param max_speed:
-        :param bus_brand:
+        Initialization attributes of parameters:
+        -number_of_transport:
+        -number_of_wheels:
+        -power_of_engine:
+        -max_speed:
+        -bus_brand:
         """
         super().__init__(number_of_transport, number_of_wheels, power_of_engine, max_speed)
         self.bus_brand = bus_brand
@@ -99,14 +103,15 @@ class Motorbike(Bicycle):
     """
     Creating slave class Motorbike from class Bicycle
     """
+
     def __init__(self, number_of_transport, number_of_wheels, power_of_engine, max_speed, motorbike_brand):
         """
-        Initialization attributes:
-        :param number_of_transport:
-        :param number_of_wheels:
-        :param power_of_engine:
-        :param max_speed:
-        :param motorbike_brand:
+        Initialization attributes of parameters:
+        -number_of_transport:
+        -number_of_wheels:
+        -power_of_engine:
+        -max_speed:
+        -motorbike_brand:
         """
         super().__init__(number_of_transport, number_of_wheels, power_of_engine, max_speed, None)
         self.motorbike_brand = motorbike_brand
@@ -115,7 +120,8 @@ class Motorbike(Bicycle):
         """
         Function returned string with attributes for object
         """
-        return f'Number of transport: {self.number_of_transport}, Number of wheels: {self.number_of_wheels}, Max speed: {self.max_speed} Km/h' \
+        return f'Number of transport: {self.number_of_transport}, Number of wheels: {self.number_of_wheels}, ' \
+               f'Power of engine: {self.power_of_engine} HP, Max speed: {self.max_speed} Km/h, ' \
                f', Motorbike Brand: {self.motorbike_brand}'
 
 
@@ -123,14 +129,15 @@ class Truck(Car):
     """
     Creating slave class Truck from master class Car
     """
+
     def __init__(self, number_of_transport, number_of_wheels, power_of_engine, max_speed, truck_brand):
         """
-        Initialization attributes:
-        :param number_of_transport:
-        :param number_of_wheels:
-        :param power_of_engine:
-        :param max_speed:
-        :param truck_brand:
+        Initialization attributes of parameters:
+        -number_of_transport:
+        -number_of_wheels:
+        -power_of_engine:
+        -max_speed:
+        -truck_brand:
         """
         super().__init__(number_of_transport, number_of_wheels, power_of_engine, max_speed, None)
         self.truck_brand = truck_brand
@@ -144,6 +151,8 @@ class Truck(Car):
                f'Truck brand: {self.truck_brand}'
 
 
+# bicycle doesn't have Number of transport and Power of Engine,
+# so we write "None" because class Motorbike inherit from class Bicycle and have both parameters
 bicycle = Bicycle(None, 2, None, 40, "BMW")
 print(bicycle)
 
--- exercise_2.py
+++ exercise_2.py
@@ -2,12 +2,13 @@ class Animals:
     """
     Creating master class of Animals
     """
+
     def __init__(self, name_of_animals, weight_of_animal, speed_of_animals):
         """
-        Initialization attributes:
-        :param name_of_animals:
-        :param weight_of_animal:
-        :param speed_of_animals:
+        Initialization attributes of parameters:
+        -name_of_animals:
+        -weight_of_animal:
+        -speed_of_animals:
         """
         self.name_of_animals = name_of_animals
         self.weight_of_animal = weight_of_animal
@@ -25,13 +26,14 @@ class Bird(Animals):
     """
     Creating slave class Bird from master class Animals
     """
+
     def __init__(self, name_of_animals, weight_of_animal, speed_of_animals, color):
         """
-        Initialization attributes:
-        :param name_of_animals:
-        :param weight_of_animal:
-        :param speed_of_animals:
-        :param color:
+        Initialization attributes of parameters:
+        -name_of_animals:
+        -weight_of_animal:
+        -speed_of_animals:
+        -color:
         """
         super().__init__(name_of_animals, weight_of_animal, speed_of_animals)
         self.color = color
@@ -48,26 +50,29 @@ class Musophagidae(Bird):
     """
     Creating slave class Musophagidae from class Bird
     """
-    def __init__(self, name_of_animals, weight_of_animal, speed_of_animals, color):
+
+    def __init__(self, name_of_animals, weight_of_animal, speed_of_animals, color, color_of_neb):
         """
-        Initialization attributes:
-        :param name_of_animals:
-        :param weight_of_animal:
-        :param speed_of_animals:
-        :param color:
+        Initialization attributes of parameters:
+        -name_of_animals:
+        -weight_of_animal:
+        -speed_of_animals:
+        -color:
+        -color_of_neb:
         """
         super().__init__(name_of_animals, weight_of_animal, speed_of_animals, color)
+        self.color_of_neb = color_of_neb
 
     def __str__(self):
         """
         Function returned string with attributes for object
         """
         main = super().__str__()
-        return main
+        return main + f', Color of neb: {self.color_of_neb}'
 
     def say(self):
         """
-        Creating function "say" for animal
+        Creating function "say" for Musophagidae
         """
         print("Voise: сch cch cch")
 
@@ -76,26 +81,29 @@ class Phoenicopteridae(Bird):
     """
     Creating slave class Phoenicopteridae from class Bird
     """
-    def __init__(self, name_of_animals, weight_of_animal, speed_of_animals, color):
+
+    def __init__(self, name_of_animals, weight_of_animal, speed_of_animals, color, length_of_legs):
         """
-        Initialization attributes:
-        :param name_of_animals:
-        :param weight_of_animal:
-        :param speed_of_animals:
-        :param color:
+        Initialization attributes of parameters:
+        -name_of_animals:
+        -weight_of_animal:
+        -speed_of_animals:
+        -color:
+        -length_of_legs:
         """
         super().__init__(name_of_animals, weight_of_animal, speed_of_animals, color)
+        self.length_of_legs = length_of_legs
 
     def __str__(self):
         """
         Function returned string with attributes for object
         """
         main = super().__str__()
-        return main
+        return main + f', Length of legs: {self.length_of_legs} cm'
 
     def say(self):
         """
-        Creating function "say" for animal
+        Creating function "say" for Phoenicopteridae
         """
         print("Voise: cke cke cke")
 
@@ -104,13 +112,14 @@ class Reptile(Animals):
     """
     Creating slave class Reptile from master class Animals
     """
+
     def __init__(self, name_of_animals, weight_of_animal, speed_of_animals, color):
         """
-        Initialization attributes:
-        :param name_of_animals:
-        :param weight_of_animal:
-        :param speed_of_animals:
-        :param color:
+        Initialization attributes of parameters:
+        -name_of_animals:
+        -weight_of_animal:
+        -speed_of_animals:
+        -color:
         """
         super().__init__(name_of_animals, weight_of_animal, speed_of_animals)
         self.color = color
@@ -127,26 +136,29 @@ class Agamidae(Reptile):
     """
     Creating slave class Agamidae from class Reptile
     """
-    def __init__(self, name_of_animals, weight_of_animal, speed_of_animals, color):
+
+    def __init__(self, name_of_animals, weight_of_animal, speed_of_animals, color, temp_of_body):
         """
-        Initialization attributes:
-        :param name_of_animals:
-        :param weight_of_animal:
-        :param speed_of_animals:
-        :param color:
+        Initialization attributes of parameters:
+        -name_of_animals:
+        -weight_of_animal:
+        -speed_of_animals:
+        -color:
+        -temp_of_body:
         """
         super().__init__(name_of_animals, weight_of_animal, speed_of_animals, color)
+        self.temp_of_body = temp_of_body
 
     def __str__(self):
         """
         Function returned string with attributes for object
         """
         main = super().__str__()
-        return main
+        return main + f', Temperature of body: {self.temp_of_body} °C'
 
     def say(self):
         """
-        Creating function "say" for animal
+        Creating function "say" for Agamidae
         """
         print("Voise: ghh ghh ghh")
 
@@ -155,26 +167,29 @@ class Testudinidae(Reptile):
     """
     Creating slave class Testudinidae from class Reptile
     """
-    def __init__(self, name_of_animals, weight_of_animal, speed_of_animals, color):
+
+    def __init__(self, name_of_animals, weight_of_animal, speed_of_animals, color, life_expectancy):
         """
-        Initialization attributes:
-        :param name_of_animals:
-        :param weight_of_animal:
-        :param speed_of_animals:
-        :param color:
+        Initialization attributes of parameters:
+        -name_of_animals:
+        -weight_of_animal:
+        -speed_of_animals:
+        -color:
+        -life_expectancy:
         """
         super().__init__(name_of_animals, weight_of_animal, speed_of_animals, color)
+        self.life_expectancy = life_expectancy
 
     def __str__(self):
         """
         Function returned string with attributes for object
         """
         main = super().__str__()
-        return main
+        return main + f', Life expectancy: {self.life_expectancy} years'
 
     def say(self):
         """
-        Creating function "say" for animal
+        Creating function "say" for Testudinidae
         """
         print("Voise: ech ech ech")
 
@@ -183,13 +198,14 @@ class Mammal(Animals):
     """
     Creating slave class Mammal from master class Animal
     """
+
     def __init__(self, name_of_animals, weight_of_animal, speed_of_animals, color):
         """
-        Initialization attributes:
-        :param name_of_animals:
-        :param weight_of_animal:
-        :param speed_of_animals:
-        :param color:
+        Initialization attributes of parameters:
+        -name_of_animals:
+        -weight_of_animal:
+        -speed_of_animals:
+        -color:
         """
         super().__init__(name_of_animals, weight_of_animal, speed_of_animals)
         self.color = color
@@ -206,26 +222,29 @@ class Sciuridae(Mammal):
     """
     Creating slave class Sciuridae from class Mammal
     """
-    def __init__(self, name_of_animals, weight_of_animal, speed_of_animals, color):
+
+    def __init__(self, name_of_animals, weight_of_animal, speed_of_animals, color, length_of_tail):
         """
-        Initialization attributes:
-        :param name_of_animals:
-        :param weight_of_animal:
-        :param speed_of_animals:
-        :param color:
+        Initialization attributes of parameters:
+        -name_of_animals:
+        -weight_of_animal:
+        -speed_of_animals:
+        -color:
+        -length_of_tail:
         """
         super().__init__(name_of_animals, weight_of_animal, speed_of_animals, color)
+        self.length_of_tail = length_of_tail
 
     def __str__(self):
         """
         Function returned string with attributes for object
         """
         main = super().__str__()
-        return main
+        return main + f', Length of tail: {self.length_of_tail} cm'
 
     def say(self):
         """
-        Creating function "say" for animal
+        Creating function "say" for Sciuridae
         """
         print("Voise: hrrr chi chi")
 
@@ -234,50 +253,53 @@ class Ursidae(Mammal):
     """
     Creating slave class Ursidae from class Mammal
     """
-    def __init__(self, name_of_animals, weight_of_animal, speed_of_animals, color):
+
+    def __init__(self, name_of_animals, weight_of_animal, speed_of_animals, color, length_of_wool):
         """
-        Initialization attributes:
-        :param name_of_animals:
-        :param weight_of_animal:
-        :param speed_of_animals:
-        :param color:
+        Initialization attributes of parameters:
+        -name_of_animals:
+        -weight_of_animal:
+        -speed_of_animals:
+        -color:
+        -length_of_wool:
         """
         super().__init__(name_of_animals, weight_of_animal, speed_of_animals, color)
+        self.length_of_wool = length_of_wool
 
     def __str__(self):
         """
         Function returned string with attributes for object
         """
         main = super().__str__()
-        return main
+        return main + f', Length of wool: {self.length_of_wool} cm'
 
     def say(self):
         """
-        Creating function "say" for animal
+        Creating function "say" for Ursidae
         """
         print("Voise: ckeeey ckeeey ")
 
 
-turaco = Musophagidae("Red-crested turaco", 0.3, 30, "green-red")
+turaco = Musophagidae("Red-crested turaco", 0.3, 30, "green-red", "yellow")
 print(turaco)
 turaco.say()
 
-flamingo = Phoenicopteridae("American flamingo", 3, 20, "purple")
+flamingo = Phoenicopteridae("American flamingo", 3, 20, "purple", 52)
 print(flamingo)
 flamingo.say()
 
-amaga = Agamidae("Sinai agama", 0.1, 15, "blue")
+amaga = Agamidae("Sinai agama", 0.1, 15, "blue", 42)
 print(amaga)
 amaga.say()
 
-turtle = Testudinidae("Floreana giant tortoise", 7, 0.001, "grey")
+turtle = Testudinidae("Floreana giant tortoise", 7, 0.001, "grey", 200)
 print(turtle)
 turtle.say()
 
-squirrel = Sciuridae("Fox squirrel", 0.15, 35, "orange-grey")
+squirrel = Sciuridae("Fox squirrel", 0.15, 35, "orange-grey", 30)
 print(squirrel)
 squirrel.say()
 
-panda = Ursidae("Giant panda", 60, 45, "black-white")
+panda = Ursidae("Giant panda", 60, 45, "black-white", 8)
 print(panda)
 panda.say()
